38ab970ebcf535f59a56a7cd315f7707d51746d7
Hervinder <hbhandal80@hotmail.com>
2021-05-04 22:13:29 +0000

    Typewriter

---
+++ typewriter.js
@@ -0,0 +1,10 @@
+const sentence = "hello there from lighthouse labs";
+const sentenceBreak = (sentence + '\n');
+let delay = 0;
+for (const char of sentenceBreak) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay);
+  delay += 100;
+}
+
